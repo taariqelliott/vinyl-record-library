f737588b28839e11f57a1140843cd7cc08060ebb
Taariq Elliott <codebytaariq@gmail.com>
2026-04-02 16:43:13 -0400

    feat: enhance ClientRecords component with dynamic filtering options for records based on selected criteria

--- frontend/src/components/ClientRecords.tsx
+++ frontend/src/components/ClientRecords.tsx
@@ -10,22 +10,54 @@ export const ClientRecords = ({
   recordsArray: RecordType[];
 }) => {
   const [searchQuery, setSearchQuery] = useState("");
-  const filteredRecords = recordsArray.filter(
-    (record) =>
-      record.album.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      record.artist.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const [filterParam, setFilterParam] = useState<keyof RecordType>("album");
+
+  const filteredRecords = recordsArray.filter((record) => {
+    const value = record[filterParam];
+
+    if (Array.isArray(value)) {
+      return value.some((item) =>
+        item.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    if (typeof value === "string") {
+      return value.toLowerCase().includes(searchQuery.toLowerCase());
+    }
+
+    if (typeof value === "number") {
+      return value.toString().includes(searchQuery);
+    }
+
+    return false;
+  });
 
   return (
     <div className="flex items-center justify-center flex-wrap my-2 flex-col w-full gap-y-3">
-      <input
-        className="mx-2 w-[calc(100%-1rem)] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-        type="text"
-        placeholder="Search for records..."
-        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          setSearchQuery(event.target.value);
-        }}
-      />
+      <div className="flex w-full">
+        <select
+          value={filterParam}
+          onChange={(e) => setFilterParam(e.target.value as keyof RecordType)}
+          className="mx-2 bg-transparent border border-slate-200 rounded-md px-3 py-2"
+        >
+          <option value="album">Album</option>
+          <option value="artist">Artist</option>
+          <option value="album_artist">Album Artist</option>
+          <option value="genre">Genre</option>
+          <option value="year">Year</option>
+          <option value="record_label">Label</option>
+          <option value="format">Format</option>
+          <option value="condition">Condition</option>
+        </select>
+        <input
+          className="mx-2 w-[calc(100%-1rem)] bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+          type="text"
+          placeholder="Search for records..."
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setSearchQuery(event.target.value);
+          }}
+        />
+      </div>
       <div className="flex flex-wrap gap-4 justify-center items-center">
         {filteredRecords.map(({ album, artist, id, album_artwork }) => (
           <Link key={id} href={`/records/${id}`} className="cursor-pointer">
